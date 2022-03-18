5e8101fbfa23e9157d6f83343d3d0155e86118b2
kusaymoo <kusaymoo@malacca.ecs.vuw.ac.nz>
2022-03-18 19:11:57 +1300

    add constructor, main, read training, read test file methods.

--- BasicMLAlgorithms/src/KNearestNeighbour.java
+++ BasicMLAlgorithms/src/KNearestNeighbour.java
@@ -1,4 +1,45 @@
+import java.io.*;
+import java.util.*;
 
 public class KNearestNeighbour {
+
+	private ArrayList<ArrayList<Double>> wineTrainingArray;
+	private ArrayList<ArrayList<Double>> wineTestArray;
+
+	public void KNearestNeighbout() {
+		this.wineTrainingArray = new ArrayList();
+		this.wineTestArray = new ArrayList();
+	}
+
+	public static void main(String[] args) {
+		String training = args[0];
+		String test = args[1];
+
+		File trainingFile = new File(training);
+		File testFile = new File(test);
+
+	}
+
+	private void readTraining(File trainFile) {
+		try {
+			Scanner trainScan = new Scanner(trainFile);
+
+
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
+	private void readTest(File testFile) {
+		try {
+			Scanner testScan = new Scanner(testFile);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
